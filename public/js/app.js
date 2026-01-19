5f33adf72a0de7a1f758236bf0b3ae6678c7c1c9
hamza-133 <hamzabouelwahab04@gmail.com>
2026-01-19 15:02:34 +0100

    feat : add section About

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,13 @@
+// * hero section
+let btnVideo = document.querySelector(".btn-video")
+btnVideo.addEventListener("click" , ()=> {
+    let contianerCarousel = document.createElement("div")
+    contianerCarousel.classList.add("contianerCarousel")
+    
+    let closeBtn = document.createElement("i")
+    closeBtn.setAttribute("class","close bi bi-x-circle")
 
+    contianerCarousel.appendChild(closeBtn)
+    document.body.appendChild(contianerCarousel)
+})
 
